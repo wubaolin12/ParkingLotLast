76c3ac618444d1a22e3f09ae3d315ea2663ae21d
wubaolin12 <494899421@qq.com>
2019-02-20 16:11:52 +0800

    吴宝林
    修改ip获取

--- ParkingLot/ParkingLot/src/org/great/log/SystemLogAspect.java
+++ ParkingLot/ParkingLot/src/org/great/log/SystemLogAspect.java
@@ -191,4 +191,32 @@ public class SystemLogAspect {
 		System.out.println("异常通知开始------------------------------------------");
 		
 	}
+	
+	/**
+	 * 	获取IP地址
+	 * @param request
+	 * @return
+	 */
+	public static String getIpAddress(HttpServletRequest request) {
+	    String ip = request.getHeader("x-forwarded-for");
+	    
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	    	ip = request.getHeader("X-Real-IP");
+	    }
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getHeader("Proxy-Client-IP");
+	    }
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getHeader("WL-Proxy-Client-IP");
+	    }
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getRemoteAddr();
+	    }
+	    if (ip.contains(",")) {
+	        return ip.split(",")[0];
+	    } else {
+	        return ip;
+	    }
+	}
+	
 }
