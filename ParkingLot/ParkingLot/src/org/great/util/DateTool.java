a97fea519bcc9086dc9bd59002cfd6a1bf443158
wubaolin12 <494899421@qq.com>
2019-02-20 10:23:28 +0800

    吴宝林

--- ParkingLot/ParkingLot/src/org/great/util/DateTool.java
+++ ParkingLot/ParkingLot/src/org/great/util/DateTool.java
@@ -1,5 +1,6 @@
 package org.great.util;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -50,28 +51,52 @@ public class DateTool {
 		
 		//返回一个Date对象
 		Date date = calendar.getTime();
-//		calendar.setTime(date);
 		String yesterday = new SimpleDateFormat("yyyy-MM-dd").format(date);
 		
 		return yesterday;
 	}
 	
+	/**
+	 * 	获取下一天的日期
+	 * @return
+	 * @throws ParseException 
+	 */
+	public static String getNextDate(String d) throws ParseException {
+		
+		//返回一个Date对象
+
+		DateFormat fm = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = fm.parse(d);
+		
+		//使用默认时区和语言环境获得一个日历
+		Calendar calendar = Calendar.getInstance();		
+		//根据日历的规则，为给定的日历字段添加或减去指定的时间量
+		calendar.setTime(date);
+		calendar.add(Calendar.DATE, +1);
+		date = calendar.getTime();
+		String nextDate = fm.format(date);
+		
+		System.out.println("下一天是=="+nextDate);
+		return nextDate;
+	}
 //	public static void main(String[] args) throws Exception {
 ////		getDate();
 //		System.out.println("---------");
 ////		getYesterdayDate();
-//		String t1 = getTime();
-//		String t2 = "2019-02-16 08:30:00";
-//		System.out.println(t1);
-//		
-//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		
-//		long m1=sdf.parse(t1).getTime()-sdf.parse(t2).getTime();
-//		
-//		if(m1>=0) {
-//			//预约到期
-//			System.out.println("预约到期");
-//		}
-//		
+////		String t1 = getTime();
+////		String t2 = "2019-02-16 08:30:00";
+////		System.out.println(t1);
+////		
+////		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+////		
+////		long m1=sdf.parse(t1).getTime()-sdf.parse(t2).getTime();
+////		
+////		if(m1>=0) {
+////			//预约到期
+////			System.out.println("预约到期");
+////		}
+//		String d = "2019-02-20";
+//		getNextDate(d);
 //	}
 }
